aee9142a8223d482c19eef05003478b8a439b8e8
Calvin Smith <hylthek@gmail.com>
2026-06-07 18:53:12 -0700

    Changed pin assignments from an enum to consts.

--- blink/pins.h
+++ blink/pins.h
@@ -1,17 +1,17 @@
-enum {
-  // SPI0 GPIOs, LT7680.
-  LCM_SCS = 5,
-  LCM_SDO = 4,
-  LCM_SDI = 3,
-  LCM_SCLK = 2,
+// consts don't use kPascalCase as to match with datasheet.
 
-  // Misc GPIOs.
-  BL_CONTROL = 7,
-  LCM_INT = 8,
-  LCM_RESET = 9,
+// SPI0 GPIOs, LT7680.
+const uint LCM_SCS = 5,
+const uint LCM_SDO = 4;
+const uint LCM_SDI = 3;
+const uint LCM_SCLK = 2;
 
-  // SPI1 GPIOs, GC9503V.
-  LCD_CS = 13,
-  LCD_DIN = 11,
-  LCD_SCLK = 10,
-};
+// Misc GPIOs.
+const uint BL_CONTROL = 7;
+const uint LCM_INT = 8;
+const uint LCM_RESET = 9;
+
+// SPI1 GPIOs; GC9503V.
+const uint LCD_CS = 13;
+const uint LCD_DIN = 11;
+const uint LCD_SCLK = 10;
