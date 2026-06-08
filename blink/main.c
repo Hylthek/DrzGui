b0d82eefc0c08aebf81c672f0731241dbb567f45
Calvin Smith <hylthek@gmail.com>
2026-06-07 18:58:25 -0700

    Added basic printf demo.

--- blink/main.c
+++ blink/main.c
@@ -1,13 +1,8 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pins.h"
 
 int main() {
-  gpio_init(25);
-  gpio_set_dir(25, GPIO_OUT);
-  while (true) {
-    gpio_put(25, true);
-    sleep_ms(250);
-    gpio_put(25, false);
-    sleep_ms(250);
-  }
+  stdio_init_all();
+  printf("Program Start\n");
 }
