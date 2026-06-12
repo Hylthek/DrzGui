fcf25f49db57ec394c462f8f2e998419f4a973d6
Calvin Smith <hylthek@gmail.com>
2026-06-11 18:03:24 -0700

    Added LCD SPIBUS functions, now will refactor.

--- blink/main.c
+++ blink/main.c
@@ -47,21 +47,21 @@ typedef enum {
 
   // Read to the register at the configured address.
   kReadData,
-} LcmCommand;
+} LcmCommandByte;
 
 // Make a SPIBUS command to the LT7680(LCM).
 // Second byte is unused for read commands.
 // Return value unused for write commands.
-uint8_t SpiCommandLcm(LcmCommand commmand, uint8_t data_to_write) {
+size_t SpiCommandLcm(LcmCommandByte commmand, size_t data_to_write) {
   // The bytes to be sent to LCM.
   // First byte is for command selection (6 bits unused)
   // Second byte is for data writes (unused for read commands).
-  uint8_t write_frame_16[2] = {0, data_to_write};
+  size_t write_frame_16[2] = {0, data_to_write};
 
   // The bytes read from the LCM.
   // First byte unused.
   // Second byte used during read commands.
-  uint8_t read_frame_16[2] = {0, 0};
+  size_t read_frame_16[2] = {0, 0};
 
   // Set command selection byte.
   switch (command) {
@@ -92,6 +92,85 @@ uint8_t SpiCommandLcm(LcmCommand commmand, uint8_t data_to_write) {
     return read_frame_16[1];
   else
     return 0x00;
+}
+
+typedef enum {
+  kSleepInBoosterOff = 0x10,
+  kSleepOutBoosterOn = 0x11,
+  kPartialModeOn = 0x12,
+  kNormalDisplayModeOn = 0x13,
+  kAllPOff = 0x22,
+  kAllPOn = 0x23,
+  kDisplayOff = 0x28,
+  kDisplayOn = 0x29,
+  kPartialStartEndAddressSet = 0x30,
+  kIdleModeOff = 0x38,
+  kIdleModeOn = 0x39,
+  kInterfacePixelFormat = 0x3A,
+  kWriteDisplayBrightness = 0x51,
+  kRgbMode = 0xB0,
+  kDisplayCtl = 0xB1,
+  kOtpCtl = 0xF6,
+  kManufactureCommandEnable = 0xF0,
+} LcdWriteCommandByte;
+
+const kLcmCommandMaxPossibleParameters = 5;
+size_t LcdCommandToParameterNum(LcdWriteCommandByte command) {
+  switch (command) {
+    case kSleepInBoosterOff:
+    case kSleepOutBoosterOn:
+    case kPartialModeOn:
+    case kNormalDisplayModeOn:
+    case kAllPOff:
+    case kAllPOn:
+    case kDisplayOff:
+    case kDisplayOn:
+    case kIdleModeOff:
+    case kIdleModeOn:
+      return 0;
+      break;
+
+    case kInterfacePixelFormat:
+    case kWriteDisplayBrightness:
+    case kDisplayCtl:
+      return 1;
+      break;
+
+    case kOtpCtl:
+      return 2;
+      break;
+
+    case kPartialStartEndAddressSet:
+      return 4;
+      break;
+
+    case kRgbMode:
+    case kManufactureCommandEnable:
+      return 5;
+      break;
+    default:
+      panic("Unexpected value.");
+      break;
+  }
+}
+
+void SpiCommandLcd(LcdWriteCommandByte command,
+                   size_t parameters[kLcmCommandMaxPossibleParameters]) {
+  uint16_t command_data = command;
+
+  size_t num_parameters = LcdCommandToParameterNum(command);
+
+  // Create parameter frames, they all need a 1 at the 9th bit (e.g. 0x100).
+  uint16_t parameter_bits[kLcmCommandMaxPossibleParameters] = {0};
+  for (int i = 0; i < num_parameters; i++)
+    parameter_bits[i] = 0x100 | parameters[i];
+
+  // Perform SPI transmission.
+  gpio_put(LCD_CS, 0);
+  spi_write16_blocking(spi1, &command_data, 1);
+  if (num_parameters > 0)
+    spi_write16_blocking(spi1, &parameter_bits, num_parameters);
+  gpio_put(LCD_CS, 1);
 }
 
 int main() {
